956717897cff71c20303d7b2cb12cbcee2303127
JenJohnston <Jen.Johnston@protonmail.ch>
2022-02-08 21:56:56 -0700

    initial framework setup
    added sass support, created pages

---
+++ dama-edmonton/src/App.jsx
@@ -0,0 +1,13 @@
+import "./css/reset.css"
+import Homepage from "./pages/HomePage"
+import "./scss/index.scss"
+
+function App() {
+  return (
+    <>
+       <Homepage />
+    </>
+  );
+}
+
+export default App;
---
+++ dama-edmonton/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Homepage() {
+    
+
+    return (
+        <>
+            <h1>I am the Home Page</h1>
+        </>
+    )
+}
