9836e69b6772b2bd58c23255280c670661fc3dce
Clayton Endersby <endersby7@gmail.com>
2022-03-03 12:59:35 -0700

    Added a bit of styles to membership page

--- dama-edmonton/src/pages/Memberships.jsx
+++ dama-edmonton/src/pages/Memberships.jsx
@@ -9,56 +9,71 @@ export default function Memberships() {
     
     
     return (
-        <div>
+        <div className='memberships-page'>
           <section>
             <h2>Why Become a Member</h2>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit nam assumenda, odit, corrupti, vitae eveniet tenetur nobis libero consequatur pariatur quas ullam perferendis rem fuga repellat inventore maxime beatae delectus.</p>
           </section>
-          <section className="individual-member">
-            <h2>Individual Membership Options</h2>
-            <div className="membership-card">
+          <section className="individual-memberships">
+            <h2>Individual Membership Options</h2><div className="membership-card">
               <div>
-                <p>$price</p>
-                <p>Student Membership</p>
+                <p>Standard Membership</p>
+                <p>$75</p>
               </div>
               <div>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi eum soluta esse excepturi omnis optio, ab voluptates fugiat reiciendis quae vero repellat id saepe atque illum perspiciatis laborum dolor. Minima?</p>
+                <p>If you purchase an annual membership you are able to attend all monthly meetings - typically 10 meetings/year and access the 15% discount on the annual conferences in Calgary and Edmonton</p>
                 <a href="#">PURCHASE</a>
               </div>
             </div>
             <div className="membership-card">
               <div>
-                <p>$price</p>
                 <p>Student Membership</p>
+                <p>$25</p>
               </div>
               <div>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi eum soluta esse excepturi omnis optio, ab voluptates fugiat reiciendis quae vero repellat id saepe atque illum perspiciatis laborum dolor. Minima?</p>
+                <p>Enjoy all the same great benefits as the standard membership. Proof of academic enrollment is required.</p>
                 <a href="#">PURCHASE</a>
               </div>
             </div>
+            
           </section>
-          <section className="corporate-member">
+          <section className="corporate-memberships">
+
             <h2>Corporate Membership Options</h2>
+
             <div className="membership-card">
               <div>
-                <p>$price</p>
-                <p>Student Membership</p>
+                <p>Up to 5 Members</p>
+                <p>$250</p>
               </div>
               <div>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi eum soluta esse excepturi omnis optio, ab voluptates fugiat reiciendis quae vero repellat id saepe atque illum perspiciatis laborum dolor. Minima?</p>
                 <a href="#">PURCHASE</a>
               </div>
             </div>
+
             <div className="membership-card">
               <div>
-                <p>$price</p>
-                <p>Student Membership</p>
+                <p>6 or more Members</p>
+                <p>$400</p>
               </div>
               <div>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi eum soluta esse excepturi omnis optio, ab voluptates fugiat reiciendis quae vero repellat id saepe atque illum perspiciatis laborum dolor. Minima?</p>
                 <a href="#">PURCHASE</a>
               </div>
             </div>
+
+            <div className="membership-card">
+              <div>
+                <p>Unlimited</p>
+                <p>$1500</p>
+              </div>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi eum soluta esse excepturi omnis optio, ab voluptates fugiat reiciendis quae vero repellat id saepe atque illum perspiciatis laborum dolor. Minima?</p>
+                <a href="#">PURCHASE</a>
+              </div>
+            </div>
+
           </section>
         </div>
     )
