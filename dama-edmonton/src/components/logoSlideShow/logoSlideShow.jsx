46019028048564a30f20d2202f5800796a2c7a40
Dan Vo <vtthanhdan.qn@gmail.com>
2022-03-04 23:00:22 -0700

    update

--- dama-edmonton/src/components/logoSlideShow/logoSlideShow.jsx
+++ dama-edmonton/src/components/logoSlideShow/logoSlideShow.jsx
@@ -1,20 +1,50 @@
-import React from "react";
+import React, {Component} from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick/lib/slider";
 import "./logoSlideShow.css";
 import LogoData from "./logodata";
 
-export default function LogoSlideShow() {
+export default class LogoSlideShow extends Component {
 
-
-    let settings = {
-        infinite: false,
-        speed: 1000,
-        arrows: true,
-        slidesToShow: 5,
-        slidesToScroll:4,
-    }
+    render() {
+        var settings = {
+          dots: true,
+          infinite: true,
+          speed: 1000,
+          slidesToShow: 5,
+          slidesToScroll: 4,
+          initialSlide: 0,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          cssEase: "linear",
+          responsive: [
+            {   
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 320,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+        };
 
     return (
         <>
@@ -27,18 +57,20 @@ export default function LogoSlideShow() {
                <Slider {...settings}>
                
                {LogoData.map(current=>(
-                <div className="out" key={current.id}>
-                    <div className="logo">
-                        <img className="logo-img" src={`assets/images/logo/${current.logoName}`} alt={`name${current.companyName}`}/>
-                    </div>
+                    <div className="out" key={current.id}>
+                        <div className="logo">
+                            <img className="logo-img" src={`assets/images/logo/${current.logoName}`} alt={`name${current.companyName}`}/>
+                        </div>
 
-                </div>
-               )
+                    </div>
+                )
                )}
                
                </Slider>
            )}
            </div>
         </>
-    )
+    );
+
+}
 }
