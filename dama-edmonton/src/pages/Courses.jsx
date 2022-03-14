0a6ee221e9d265e52e7fb9eb6186bc19b615615b
Dan Vo <vtthanhdan.qn@gmail.com>
2022-03-14 13:06:05 -0600

    update

--- dama-edmonton/src/pages/Courses.jsx
+++ dama-edmonton/src/pages/Courses.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+/*
 import axios from 'axios';
 
 class Courses extends React.Component {
@@ -39,3 +42,92 @@ class Courses extends React.Component {
 }
 
 export default Courses;
+*/
+
+
+import FeedBackForm from '../components/feedbackForm/feedbackForm';
+import CallIcon from '@mui/icons-material/Call';
+import EmailIcon from '@mui/icons-material/Email';
+
+
+export default function Course() {
+    
+
+    return (
+      
+        <>
+        <div className="course-page-wrapper">
+          <div className='course-banner' style={{background: `linear-gradient(rgba(255,255,255,0.6),
+            rgba(2255,255,255,0.6) ), url("assets/images/profile-picture/background.jpg") top/cover no-repeat` }}>
+            <h1>TRAINING, EDUCATION, CERTIFICATION</h1>
+          </div>
+          <div className='search-form'>
+
+            <button>Courses</button>
+            <input type="text" name="search-bar" />
+            <input type="submit" name="search-submit" id="search-submit" />
+            
+          </div>
+          <div className='main-container'>
+            <div className='sidebar'>
+              <h2>Online Courses</h2>
+              <ul>
+                <li><a href="#">Data Governance and Stewardship (Certified DGS Professional)</a></li>
+                <li><a href="#">Data Foundations/Data Literacy (Certified Business Intelligence Professional (CBIP, or Certified Data Professional - CDP))</a></li>
+                <li><a href="#">Data Management (Prepare for the CDMP )</a></li>
+                <li><a href="#">Data Modeling</a></li>
+                <li><a href="#">Certified Business Intelligence Professional (CBIP, or Certified Data Professional-CDP)</a></li>
+                <li><a href="#">Information Systems (Certified Business Intelligence Professional (CBIP, or Certified Data Professional))</a></li>
+                <li><a href="#">Business Intelligence and Data Analytics: (Certified Business Intelligence Professional (CBIP, or Certified Data Professional))</a></li>
+              </ul>
+
+            </div>
+
+            <div className='content-detail'>
+              <div className='course-card'>
+                <h2>Self Study Courses</h2>
+                <ul>
+                    <li>$595</li>
+                    <li>Start at Any Time</li>
+                    <li>Work at your own pace</li>
+                    <li>Ideal for Experienced Data Professionals</li>
+                </ul>
+                <Link to="#">Learn More &#62;</Link>
+              </div>
+
+              <div className='course-card'>
+                <h2>Instrutor-Led, Group Study-paced Learning</h2>
+               
+                <ul>
+                  <li>$1295</li>
+                  <li>Live Sessions </li>
+                  <li>Cohort Paced, 12 week program</li>
+                  <li>One hour weekly live discussion and Q&#38;A</li>
+                </ul>
+                <a href="#">More infor</a>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div className='certification-content'>
+            <h2>Certification Examinations</h2>
+          
+              <ul>
+                <li><a href="#"><CallIcon className='call-icon'/> 1.800.843.8227 </a> to schedule a date and time to take the Data Governance and Stewardship Professional examination or </li>
+                <li><a href="#"><EmailIcon className='email-icon'/> damaedmonton@gmail.com </a> with your preferred dates for an online proctored examination. </li>
+              </ul>
+          </div>
+
+        </div>
+
+        <div className='feedback'>
+          <FeedBackForm/>
+        </div>
+        </>
+    )
+}
+
+
+
--- dama-edmonton/src/pages/Events.jsx
+++ dama-edmonton/src/pages/Events.jsx
@@ -1,7 +1,10 @@
 
 import React  from 'react'
 
-
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import RemoveIcon from '@mui/icons-material/Remove';
+import Eventcard from '../components/eventCard/EventCard';
+import FeedBackForm from '../components/feedbackForm/feedbackForm';
 
 export default function Events() {
 
@@ -9,11 +12,161 @@ export default function Events() {
     
     return (
         <>
-              
-            <h1>Events</h1>
-            
-            
+        <div className='event-page-wrapper'>
+            <div className='course-banner' style={{background: `linear-gradient(rgba(255,255,255,0.6),
+                rgba(2255,255,255,0.6) ), url("assets/images/profile-picture/background.jpg") top/cover no-repeat` }}>
+                <h1>EVENTS - PAST AND PRESENT</h1>
+            </div>
+            <div className='search-form'>
+
+                <button>Events</button>
+                <input type="text" name="search-bar" />
+                <input type="submit" name="search-submit" id="search-submit" />
+                
+            </div>
+            <div className='main-content'>
+                <div className='sidebar'>
+                    <div className='upcoming-event'>
+                        <div className='main-title'>
+                            <h2>Upcoming Events</h2>
+                            <ArrowDropDownIcon classname="arrowdown-icon"/>
+                        </div>
+                        <div className='event-detail'>
+                            <div className='title'>
+                                <h3>Amelia Estwick</h3>
+                                <RemoveIcon classname="remove-icon"/>
+                            </div>
+                            <div className='topic-content'>
+                               <p>Topic: How to talk to the business side about ML</p>
+                               <p>Time: 5:00 p.m April 21, 2022</p>
+                                <a href="#">More infor</a>
+                            </div>
+                            
+                        </div>
+                        <div className='event-detail'>
+                            <div className='title'>
+                                <h3>Joseph Paul</h3>
+                                <RemoveIcon classname="remove-icon"/>
+                            </div>
+                            <div className='topic-content'>
+                               <p>Topic: How to talk to the business side about ML</p>
+                               <p>Time: 5:00 p.m April 21, 2022</p>
+                                <a href="#">More infor</a>
+                            </div>
+                            
+                        </div>
+                        <div className='event-detail'>
+                            <div className='title'>
+                                <h3>William Inmon</h3>
+                                <RemoveIcon classname="remove-icon"/>
+                            </div>
+                            <div className='topic-content'>
+                               <p>Topic: How to talk to the business side about ML</p>
+                               <p>Time: 5:00 p.m April 21, 2022</p>
+                                <a href="#">More infor</a>
+                            </div>
+                            
+                        </div>
+                        <div className='event-detail'>
+                            <div className='title'>
+                                <h3>Karen Parker</h3>
+                                <RemoveIcon classname="remove-icon"/>
+                            </div>
+                            <div className='topic-content'>
+                               <p>Topic: How to talk to the business side about ML</p>
+                               <p>Time: 5:00 p.m April 21, 2022</p>
+                                <a href="#">More infor</a>
+                            </div>
+                            
+                        </div>
+                        <div className='event-detail'>
+                            <a href="#">More</a>
+
+                        </div>
+                       
+                    </div>
+
+                    <div  className='main-title'>
+                        <div>
+                            <h2>Past events</h2>
+                            <ArrowDropDownIcon classname="arrowdown-icon"/>
+                        </div>
+                        <div className='event-detail'>
+                            <div className='title'>
+                                <h3>Amelia Estwick</h3>
+                                <RemoveIcon classname="remove-icon"/>
+                            </div>
+                            <div className='topic-content'>
+                               <p>Topic: How to talk to the business side about ML</p>
+                               <p>Time: 5:00 p.m April 21, 2022</p>
+                                <a href="#">Presentation Slide</a>
+                                <a href="#">document.PDF</a>
+                            </div>
+                            
+                        </div>
+                        <div className='event-detail'>
+                            <div className='title'>
+                                <h3>Joseph Paul</h3>
+                                <RemoveIcon classname="remove-icon"/>
+                            </div>
+                            <div className='topic-content'>
+                               <p>Topic: How to talk to the business side about ML</p>
+                               <p>Time: 5:00 p.m April 21, 2022</p>
+                               <a href="#">Presentation Slide</a>
+                                <a href="#">document.PDF</a>
+                            </div>
+                            
+                        </div>
+                        <div className='event-detail'>
+                            <div className='title'>
+                                <h3>William Inmon</h3>
+                                <RemoveIcon classname="remove-icon"/>
+                            </div>
+                            <div className='topic-content'>
+                               <p>Topic: How to talk to the business side about ML</p>
+                               <p>Time: 5:00 p.m April 21, 2022</p>
+                               <a href="#">Presentation Slide</a>
+                                <a href="#">document.PDF</a>
+                            </div>
+                            
+                        </div>
+                        <div className='event-detail'>
+                            <div className='title'>
+                                <h3>Karen Parker</h3>
+                                <RemoveIcon classname="remove-icon"/>
+                            </div>
+                            <div className='topic-content'>
+                               <p>Topic: How to talk to the business side about ML</p>
+                               <p>Time: 5:00 p.m April 21, 2022</p>
+                               <a href="#">Presentation Slide</a>
+                                <a href="#">document.PDF</a>
+                            </div>
+                            
+                        </div>
+                        <div className='event-detail'>
+                            <a href="#">More</a>
+
+                        </div>
+                       
+                    </div>
+
+                </div>
+
+                <div className='event-list'>
+                    <h2>Upcoming Events</h2>
+                    <div className='event-flex'>
+                        <Eventcard/>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div className='feedback'>
+                <FeedBackForm/>
+            </div>
         
+        </div>  
         </>
     )
 }
