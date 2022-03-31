fbba84abff464c4cc70cd62227061b3e693f81f4
JenJohnston <73721221+JenJohnston@users.noreply.github.com>
2022-03-30 22:46:07 -0600

    started checkout form

--- dama-edmonton/src/pages/CheckOut.jsx
+++ dama-edmonton/src/pages/CheckOut.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import CartContext from '../context/cart/CartContext'
+import CurrencyFormat from 'react-currency-format';
 
 
 export default function Checkout() {
     
 
     return (
-        <>
-             
-            <h1>Check Out</h1>
-            
-        </>
+        <div className='checkout-container'>
+            <div className="checkout-wrapper">
+                <h2>Check Out</h2>
+                <div className="checkout-form">
+                    <form action="POST" name="checkoutForm" className='checkout-form'>
+                        <div className="contact-form-title">
+                            <h3>Customer Information</h3>
+                        </div>
+                        <div className="form-control">
+                            <label htmlFor="firstname" className='form-label'> <span className="required">*</span>First Name</label>
+                            <input type="text" name="first-name" className='form-input' aria-required={true} required/>
+                        </div>
+                        <div className="form-control">
+                            <label htmlFor="lastname" className='form-label'> <span className="required">*</span>Last Name</label>
+                            <input type="text" name="lastname"  className='form-input' aria-required={true} required/>
+                        </div>
+                        <div className="form-control">
+                            <label htmlFor="phoneNumber" className='form-label'> <span className="required">*</span>Phone number</label>
+                            <input type="tel:" name="phoneNumber"  className='form-input' aria-required={true} required value="(123) 456-7890"/>
+                        </div>
+                        <div className="form-control">
+                            <label htmlFor="email" className='form-label'> <span className="required">*</span>Last Name</label>
+                            <input type="mailto:" name="email"  className='form-input' aria-required={true} required value="janesmith@gmail.com"/>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
