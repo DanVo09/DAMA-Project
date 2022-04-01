0d1f0254c1ada80279ecd7b487153f1ecec07022
Clayton Endersby <endersby7@gmail.com>
2022-04-01 16:59:30 -0600

    finished dashboard and membership styles

--- dama-edmonton/src/pages/Memberships.jsx
+++ dama-edmonton/src/pages/Memberships.jsx
@@ -14,63 +14,67 @@ export default function Memberships() {
             <section className="individual-memberships memberships">
               <h2>Individual Membership Options</h2>
 
-              <div className="membership-card">
-                  <div>
-                    <h3>Standard Membership</h3>
-                    <p>$75</p>
-                  </div>
-                  <p>If you purchase an annual membership you are able to attend all monthly meetings - typically 10 meetings/year and access the 15% discount on the annual conferences in Calgary and Edmonton</p>
-                  <div className='purchase'>
-                    <Link to="#" className='purchase-membership'>PURCHASE</Link>
-                  </div>
-              </div>
-
-              <div className="membership-card">
-                  <div>
-                    <h3>Student Membership</h3>
-                    <p>$25</p>
-                  </div>
-                  <p>Enjoy all the same great benefits as the standard membership. Proof of academic enrollment is required.</p>
-                  <div className='purchase'>
-                    <Link to="#" className='purchase-membership'>PURCHASE</Link>
-                  </div>
+              <div className='membership-cards'>
+                <div className="membership-card">
+                    <div>
+                      <h3>Standard Membership</h3>
+                      <p>$75</p>
+                    </div>
+                    <p className='membership-description'>If you purchase an annual membership you are able to attend all monthly meetings - typically 10 meetings/year and access the 15% discount on the annual conferences in Calgary and Edmonton</p>
+                    <div className='purchase'>
+                      <Link to="#" className='purchase-membership'>PURCHASE</Link>
+                    </div>
+                </div>
+                
+                <div className="membership-card">
+                    <div>
+                      <h3>Student Membership</h3>
+                      <p>$25</p>
+                    </div>
+                    <p className='membership-description'>Enjoy all the same great benefits as the standard membership. Proof of academic enrollment is required.</p>
+                    <div className='purchase'>
+                      <Link to="#" className='purchase-membership'>PURCHASE</Link>
+                    </div>
+                </div>
               </div>
             </section>
 
             <section className="corporate-memberships memberships">
               <h2>Corporate Membership Options</h2>
 
-              <div className="membership-card">
-                  <div>
-                    <h3>Up to 5 Members</h3>
-                    <p>$250</p>
-                  </div>
-                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi eum soluta esse excepturi omnis optio, ab voluptates fugiat reiciendis quae vero repellat id saepe atque illum perspiciatis laborum dolor. Minima?</p>
-                  <div className='purchase'>
-                    <Link to="#" className='purchase-membership'>PURCHASE</Link>
-                  </div>
-              </div>
-
-              <div className="membership-card">
-                  <div>
-                    <h3>6 or more Members</h3>
-                    <p>$400</p>
-                  </div>
-                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi eum soluta esse excepturi omnis optio, ab voluptates fugiat reiciendis quae vero repellat id saepe atque illum perspiciatis laborum dolor. Minima?</p>
-                  <div className='purchase'>
-                    <Link to="#" className='purchase-membership'>PURCHASE</Link>
-                  </div>
-              </div>
-
-              <div className="membership-card">
-                  <div>
-                    <h3>Unlimited</h3>
-                    <p>$1500</p>
-                  </div>
-                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi eum soluta esse excepturi omnis optio, ab voluptates fugiat reiciendis quae vero repellat id saepe atque illum perspiciatis laborum dolor. Minima?</p>
-                  <div className='purchase'>
-                    <Link to="#" className='purchase-membership'>PURCHASE</Link>
-                  </div>
+              <div className='membership-cards'>
+                <div className="membership-card">
+                    <div>
+                      <h3>Up to 5 Members</h3>
+                      <p>$250</p>
+                    </div>
+                    <p className='membership-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi eum soluta esse excepturi omnis optio, ab voluptates fugiat reiciendis quae vero repellat id saepe atque illum perspiciatis laborum dolor. Minima?</p>
+                    <div className='purchase'>
+                      <Link to="#" className='purchase-membership'>PURCHASE</Link>
+                    </div>
+                </div>
+                
+                <div className="membership-card">
+                    <div>
+                      <h3>6 or more Members</h3>
+                      <p>$400</p>
+                    </div>
+                    <p className='membership-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi eum soluta esse excepturi omnis optio, ab voluptates fugiat reiciendis quae vero repellat id saepe atque illum perspiciatis laborum dolor. Minima?</p>
+                    <div className='purchase'>
+                      <Link to="#" className='purchase-membership'>PURCHASE</Link>
+                    </div>
+                </div>
+                
+                <div className="membership-card">
+                    <div>
+                      <h3>Unlimited</h3>
+                      <p>$1500</p>
+                    </div>
+                    <p className='membership-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi eum soluta esse excepturi omnis optio, ab voluptates fugiat reiciendis quae vero repellat id saepe atque illum perspiciatis laborum dolor. Minima?</p>
+                    <div className='purchase'>
+                      <Link to="#" className='purchase-membership'>PURCHASE</Link>
+                    </div>
+                </div>
               </div>
             </section>
 
