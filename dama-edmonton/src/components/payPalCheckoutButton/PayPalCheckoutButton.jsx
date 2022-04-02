a3461cc0748a1b2ec8297e62fa69be27bf26637e
JenJohnston <73721221+JenJohnston@users.noreply.github.com>
2022-04-01 18:59:27 -0600

    added .env file and paypal component

---
+++ dama-edmonton/src/components/payPalCheckoutButton/PayPalCheckoutButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { PayPalButtons } from '@paypal/react-paypal-js'
+
+export default function Paypalcheckoutbutton(props) {
+    const {product} = props;
+
+    return (
+
+        <PayPalButtons />
+            
+    )
+}
