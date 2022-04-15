de770e1b56fe95e8dafd21a6ebfa2b289978515b
Dan Vo <vtthanhdan.qn@gmail.com>
2022-04-15 00:47:13 -0600

    update

--- dama-edmonton/src/components/jobs/update.jsx
+++ dama-edmonton/src/components/jobs/update.jsx
@@ -1,94 +1,57 @@
-import React, {Component }  from "react";
+import React, {useState, useEffect }  from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import Swal from "sweetalert2";
 
 
 
-class UpdateAJob extends Component{
-    state = {
-       
-        job_title: '',
-        company_name: '',
-        location: '',
-        job_desc:'',
-        error_list:[],
-        user_id: Number(JSON.parse(localStorage.getItem("user-info")).map(obj => obj.user_id)),
-     
-    }
+function UpdateJob() {
+
+  
+    const [jobdata, setjobdata] = useState([]);
+
+    const {posting_id} = useParams();
+
+    useEffect(async ()=> {
+        let result = await fetch(`http://dama.web.dmitcapstone.ca/api/update_job?id=${posting_id}`);
+        result = await result.json();
+        setjobdata(result);
+
+    }, [posting_id])
+
+    console.log(jobdata)
 
 
-    
+    return(
 
-    handleInput = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        });
-    }
-
-    async componentDidMount() {
-
-        const id = this.props.match.params.id;
-
-        console.log(id)
-
-    }
-
-
-    updateJob = async (e) => {
-        e.preventDefault();
-        const res = await axios.post("http://dama.web.dmitcapstone.ca/api/job_updated/", this.state);
-        if(res.data.validate_err)
-        {
-            this.setState({
-                error_list: res.data.validate_err,
-                
-           }); 
-        } else {
-            Swal.fire({  
-                icon: 'success',  
-                title: 'Successful!',  
-                text: res.data.message,  
-              }); 
-
-        }
-    
-
-}
-
-    render(){
-        return(
-            <div className="form-wrapper">
-            <form onSubmit={this.updateJob} className="login-form-container">
+        <div className="form-wrapper">
+          <form  className="login-form-container">
             <h1>Edit a Job</h1>
-                <div className="form">
+            <p>Title: {jobdata.job_title}</p>
+                    <div className="form">
+                        
                     <label htmlFor="job_title">Job Title</label>
-                    <input type="text" id="job_title" onChange={this.handleInput} value={this.setState.job_title} name="job_title"/>
-                    <span className='error'>{this.state.error_list.job_title}</span>
+                    <input type="text" value={jobdata.job_title}  name="job_title"/>
+                    <span className='error'></span>
                     <label htmlFor="company_name">Company Name</label>
-                    <input type="text" id="company_name" onChange={this.handleInput} value={this.setState.company_name} name="company_name"  />
-                    <span className='error'>{this.state.error_list.company_name}</span>
+                    <input type="text" value={jobdata.company_name} name="company_name"  />
 
                     <label htmlFor="location">Location</label>
-                    <input type="text" id="location" onChange={this.handleInput} value={this.setState.location} name="location"  />
-                    <span className='error'>{this.state.error_list.location}</span>
+                    <input type="text" value={jobdata.location}  name="location"  />
+                    <span className='error'></span>
 
                     <label htmlFor="job_desc">Job Description</label>
-                    <textarea name="job_desc" onChange={this.handleInput} id="job_desc">{this.setState.job_desc}</textarea>
-                    <span className='error'>{this.state.error_list.job_desc}</span>
+                    <textarea name="job_desc" value={jobdata.job_desc}  ></textarea>
+                    <span className='error'></span>
 
                     <button type='submit'>Update Job</button>
                 </div>
-               
             </form>
              
         </div>
-        );
-    }
-}
-
-
-export default function UpdateJob() {
-    return (
-        <UpdateAJob />
+        
+           
     )
 }
+
+export default UpdateJob;
